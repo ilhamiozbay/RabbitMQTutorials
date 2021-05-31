161f0349e1cb1f71bda6d63161ab92c38f7db1a9
İlhami ÖZBAY <ilhami.ozbay@gmail.com>
2021-06-01 00:37:18 +0300

    first HelloWorld

---
+++ HelloWorld/HelloWorld.Receive/Receive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace HelloWorld.Receive
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    Console.WriteLine(" [*] Waiting for messages.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.Send/Send.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace HelloWorld.Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            do
+            {
+                Console.Write("Type the message to be sent: ");
+                message = Console.ReadLine();
+                SendToRabbitMQ(message);
+                Console.WriteLine(" Write [exit] to quit.");
+            } while (message.Trim().ToLower() != "exit");
+
+        }
+
+        private static void SendToRabbitMQ(string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+    }
+}
