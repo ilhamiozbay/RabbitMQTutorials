6a7c4be453d67ec78c9993cc358f914016da101c
İlhami ÖZBAY <ilhami.ozbay@gmail.com>
2021-06-19 14:18:51 +0300

    4 - routing

---
+++ Routing/Routing.ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Routing.ReceiveLogsDirect
+{
+    class ReceiveLogsDirect
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    if (args.Length < 1)
+                    {
+                        Console.Error.WriteLine("");
+                        Console.WriteLine("");
+                        Console.ReadLine();
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+
+                    foreach (var severity in args)
+                    {
+                        channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: severity);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for messages.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        var routingKey = ea.RoutingKey;
+                        Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+                    };
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
